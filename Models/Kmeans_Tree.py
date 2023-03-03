17bd4cf518f1d12d194ac2830e6583c6ac52c308
D0men1c0 <d.lacavalla2@studenti.uniba.it>
2023-03-03 01:55:58 +0100

    Terminata Classificazione

--- Models/Kmeans_Tree.py
+++ Models/Kmeans_Tree.py
@@ -1,7 +1,9 @@
-from sklearn.cluster import DBSCAN, KMeans
+from sklearn.cluster import KMeans
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+# Parte Clustering
 
 # Caricamento dei dati
 data = pd.read_csv("dataset/nofeat_acqua.csv")
@@ -17,10 +19,8 @@ kmeans = KMeans(n_clusters=6, random_state=42)
 label = kmeans.fit_predict(X)
 
 # Aggiunta delle etichette come una nuova colonna al dataframe di dati
-data["etichette"] = label
+data["Etichetta"] = label
 data.to_csv('dataset/acqua_etichette.csv', index=False)
-
-
 
 '''
 # Number of clusters in labels, ignoring noise if present.
@@ -31,3 +31,19 @@ print("Estimated number of clusters: %d" % n_clusters_)
 print("Estimated number of noise points: %d" % n_noise_)
 '''
 
+# Parte Classifier
+
+# Caricamento dei dati etichettati
+data = pd.read_csv("dataset/acqua_etichette.csv")
+X_labeled = data[['Durata', 'Consumo']]
+y_labeled = data[['Etichetta']]
+
+# Divisione dei dati in training set e test set
+X_train, X_test, y_train, y_test = train_test_split(X_labeled, y_labeled, test_size=0.2, random_state=42)
+
+# Creazione di un'istanza di DecisionTreeClassifier e addestramento del modello
+model = DecisionTreeClassifier(criterion = 'gini', max_depth = 15, max_features = 'sqrt', min_samples_split = 2, random_state = 36, splitter = 'best', max_leaf_nodes = 20)
+model.fit(X_train, y_train)
+
+# Valutazione della performance del modello sul test set
+print(model.score(X_test, y_test))
